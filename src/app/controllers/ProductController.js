ee428e9f783c4151d7656836e1b2c2a9373895ab
Jony Reis <jonyreiscardoso@gmail.com>
2020-01-17 23:17:49 -0300

    Add edit page

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,5 +1,9 @@
+const { formatPrice } = require('../../lib/utils')
+
+
 const Category = require('../models/Category')
 const Product = require('../models/Product')
+
 
 module.exports = {
     create(req, res) {
@@ -16,7 +20,7 @@ module.exports = {
 
     },
     async post(req, res) {
-        // Lógica de salvar
+
         const keys = Object.keys(req.body)
 
         for (key of keys) {
@@ -28,10 +32,22 @@ module.exports = {
         let results = await Product.create(req.body)
         const productId = results.rows[0].id
 
+        return res.redirect(`/products/${productId}`)
+
+    },
+    async edit(req, res) {
+        let results = await Product.find(req.params.id)
+        const product = results.rows[0]
+
+
+        if(!product) return res.send('Product not found!')
+
+        product.old_price = formatPrice(product.old_price)
+        product.price = formatPrice(product.price)
+
         results = await Category.all()
         const categories = results.rows
 
-        return res.render('products/create', { productId, categories })
-
+        return res.render('products/edit', { product , categories })
     }
 }
